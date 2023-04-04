d8bb5ab85862f7e4f20ce865570acb2c292749d8
ittang_majoo <ahmad.faisal@majoo.id>
2023-04-04 09:06:26 +0700

    done flutter chat app

---
+++ lib/models/profile_model.dart
@@ -0,0 +1,21 @@
+class ProfileModel {
+  ProfileModel({
+    required this.id,
+    required this.username,
+    required this.createdAt,
+  });
+
+  /// User ID of the profile
+  final String id;
+
+  /// Username of the profile
+  final String username;
+
+  /// Date and time when the profile was created
+  final DateTime createdAt;
+
+  ProfileModel.fromMap(Map<String, dynamic> map)
+      : id = map['id'],
+        username = map['username'],
+        createdAt = DateTime.parse(map['created_at']);
+}
